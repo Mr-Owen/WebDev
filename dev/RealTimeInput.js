a0fe0198ca0fc334ad7a7608c58b5aadb657dcd7
Bowie <mr_lbw@outlook.com>
2018-01-18 17:23:59 +0800

    实时显示输入字符

---
+++ dev/RealTimeInput.js
@@ -0,0 +1,19 @@
+/* jslint esversion: 6 */
+let userName = (event) => {
+  let setInputName = document.getElementById("search-box").value;
+  // real time simulator
+  document.querySelector('.outputarea').innerText = setInputName.toString();
+  // submit text value
+  if (event.keyCode == 13) {
+    document.querySelector('.outputarea').innerText = '你输入的是：' + setInputName.toString();
+  }
+};
+document.body.addEventListener('keyup', userName, false);
+
+// 实现效果：
+// 1.实时显示输入字符
+// 2.回车提交并显示输入的内容
+
+// 总结：
+// 1.keyup显示的是实时输入结果，无法过滤(阻止)输入
+// 2.keydown和keypress显示的是上一步的结果，可用于过滤(阻止)输入
