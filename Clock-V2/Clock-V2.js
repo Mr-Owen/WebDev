2b929f0096646c77c64c82ca6e9724cbc1d1fc35
Bwow <mr_lbw@outlook.com>
2018-01-18 12:13:55 +0800

    Update point

--- Clock-V2/Clock-V2.js
+++ Clock-V2/Clock-V2.js
@@ -12,29 +12,31 @@ let num = [
   [0, 5, 6, 1, 2, 3]
 ];
 
-let setTime = function(digit, nowTime) {
+let setTime = (digit, nowTime) => {
   let part = digit.querySelectorAll('.part');
   let current = parseInt(digit.getAttribute('data-value'));
   if (!isNaN(current) && current != nowTime) {
-    num[current].forEach(function(num) {
+    num[current].forEach((num) => {
       part[num].classList.remove('on');
     });
   }
   if (isNaN(current) || current != nowTime) {
-    num[nowTime].forEach(function(num) {
-      part[num].classList.add('on');
+    num[nowTime].forEach((num, index) => {
+      setTimeout(() => {
+        part[num].classList.add('on');
+      }, index * 50);
     });
   }
   digit.setAttribute('data-value', nowTime);
 };
 
 // trigger
-let trigger = function(event) {
+let trigger = (event) => {
   let _hour = document.querySelectorAll('.hour');
   let _min = document.querySelectorAll('.min');
   let _sec = document.querySelectorAll('.sec');
   let now;
-  setInterval(function() {
+  setInterval(() => {
     now = /(\d)(\d):(\d)(\d):(\d)(\d)/.exec(new Date());
     setTime(_hour[0], now[1]);
     setTime(_hour[1], now[2]);
@@ -42,6 +44,17 @@ let trigger = function(event) {
     setTime(_min[1], now[4]);
     setTime(_sec[0], now[5]);
     setTime(_sec[1], now[6]);
+    if (now[6] % 2 != 0) {
+      [0,1].forEach((number) => {
+        document.querySelectorAll('.point')[number].classList.remove('on');
+        document.querySelectorAll('.point1')[number].classList.remove('on');
+      });
+    }else{
+      [0,1].forEach((number) => {
+        document.querySelectorAll('.point')[number].classList.add('on');
+        document.querySelectorAll('.point1')[number].classList.add('on');
+      });
+    }
   }, 1000);
 
 };
