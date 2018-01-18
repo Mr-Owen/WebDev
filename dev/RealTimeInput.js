2844481b715f3a1af001f9154474f475c744cab2
Bowie <mr_lbw@outlook.com>
2018-01-18 17:39:21 +0800

    实时显示输入字符

--- dev/RealTimeInput.js
+++ dev/RealTimeInput.js
@@ -5,14 +5,18 @@ let userName = (event) => {
   document.querySelector('.outputarea').innerText = setInputName.toString();
   // submit text value
   if (event.keyCode == 13) {
-    document.querySelector('.outputarea').innerText = '你输入的是：' + setInputName.toString();
+    if (!setInputName) {
+      document.querySelector('.outputarea').innerText = 'Oops!!你什么也没有输入';
+    } else {
+      document.querySelector('.outputarea').innerText = '你输入的是：' + setInputName.toString();
+    }
   }
 };
 document.body.addEventListener('keyup', userName, false);
 
 // 实现效果：
 // 1.实时显示输入字符
-// 2.回车提交并显示输入的内容
+// 2.回车提交并显示输入的内容，若确认时无输入内容则提醒
 
 // 总结：
 // 1.keyup显示的是实时输入结果，无法过滤(阻止)输入
