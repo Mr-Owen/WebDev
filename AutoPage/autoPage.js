a993b03fa1516920def0bec23c9f370e581becf1
Bowie <mr_lbw@outlook.com>
2018-01-26 00:21:40 +0800

    Singleton pattern

--- AutoPage/autoPage.js
+++ AutoPage/autoPage.js
@@ -5,7 +5,8 @@
 //   1.1 在第一页时，点击prev回到末尾，反之进入下一页；在末页时，点击next回到第一页，反之回到上一页
 //   1.2 处在中间的按照方向的不同进行加或减操作
 // 2.小圆点（每个原点都有一个data-value表示第几张图片）
-//   2.1 不点击小圆点的情况下，与翻页时的加减操作相同，并且转到相应页面给相应的标签添加特殊的类("on")
+//   2.1 不点击小圆点的情况下，用修改后的left属性除以图片宽度，这样可得到绝对的图像位置，将图像和索引紧密联系在了一起，然后给相应的标签添加特殊的类("on")
+//   note:之前是直接加减而不是取绝对图像位置，容易在点击圆点和箭头之间出现慢一步的情况，故这里改为求绝对图像位置
 //   2.2 点击小圆点时，取得自定义data-value值，将得到的自定义值减一乘以负图像宽度（即展示第几张图），将得到的结果赋值给图像区的left属性，达到点击圆点翻页的目的
 // 3. 定时器
 //   3.1 mouseover事件时，取消定时器；mouseout事件时添加定时器
@@ -17,87 +18,94 @@
 // 3. 点击底部小圆点翻页
 
 // 以下已将特殊值转换为变量，在修改html图片数量、大小时，不用修改js数据
-// 已设置list类的宽度为1200px，可调节该宽度，保证有list类宽度/imgWidth == spanLen成立，否则多余的图将显示为空白
-(function() {
+// 已设置list容器的宽度为1200px，可调节该宽度，保证有list类宽度/img_width == img_len成立，否则多余的图将显示为空白
+
+let get_animate = (() => {
   let list = document.querySelector(".list"),
-    content = document.querySelector(".content"),
+    box = document.querySelector(".content"),
     span = document.querySelectorAll("span"),
-    prev = document.querySelector(".prev"),
-    next = document.querySelector(".next"),
-    spanLen = document.querySelectorAll("img").length,
-    imgWidth = document.querySelector("img").width,
-    maxLeft = -(spanLen - 1) * imgWidth,
-    timer = 0,
-    index = 1; // 只要图片变换，index一定要变换
-  let animate = (num, target) => {
-    // 翻页
-    let _array = parseInt(list.style.left, 10);
-    if (_array == 0) {
-      if (target == "prev") {
-        list.style.left = maxLeft + "px";
-        index = spanLen;
-      } else {
-        list.style.left = _array + num + "px";
+    img_len = document.querySelectorAll("img").length,
+    img_width = document.querySelector("img").width,
+    max_left = -(img_len - 1) * img_width,
+    img_timer = 0,
+    img_index = 1;
+  return {
+    _box: box,
+    _list: list,
+    _timer: img_timer,
+    _width: img_width,
+    animate: (num, tg) => {
+      // 翻页
+      let digit = parseInt(list.style.left, 10);
+      switch (digit) {
+        case 0:
+          if (tg == "prev") {
+            list.style.left = max_left + "px";
+          } else {
+            list.style.left = digit + num + "px";
+          }
+          img_index = -parseInt(list.style.left, 10) / img_width + 1; // 取left计算后的值，所以不能取digit,+1防止负0出现
+          break;
+
+        case max_left:
+          if (tg == "prev") {
+            list.style.left = digit + num + "px";
+          } else {
+            list.style.left = 0 + "px";
+          }
+          img_index = -parseInt(list.style.left, 10) / img_width + 1;
+          break;
+
+        default:
+          list.style.left = digit + num + "px";
+          img_index = -parseInt(list.style.left, 10) / img_width + 1;
       }
-    }
-    if (_array == maxLeft) {
-      if (target == "prev") {
-        list.style.left = _array + num + "px";
-      } else {
-        list.style.left = 0 + "px";
-        index = 1;
+    },
+
+    point: (item) => {
+      if (typeof item !== "undefined") {
+        img_index = item; // 最好在函数内修改私有变量
       }
-    }
-    if (_array > maxLeft && _array < 0) {
-      list.style.left = _array + num + "px";
+      for (let i = 0; i < img_len; i++) {
+        span[i].classList.remove("on");
+      }
+      span[img_index - 1].classList.add("on");
     }
   };
-
-  let point = (index) => {
-    for (let i = 0; i < spanLen; i++) {
-      span[i].classList.remove("on");
-    }
-    span[index - 1].classList.add("on");
-  };
-
-  // Event listener
-  window.addEventListener("load", function(event) {
-    timer = setInterval(function() {
-      index += 1;
-      animate(-1 * imgWidth);
-      point(index);
-    }, 3000);
-  }, false);
-
-  content.addEventListener("click", function(event) {
-    target = event.target.classList[0];
-    if (target == "prev") {
-      index -= 1;
-      animate(imgWidth, target);
-      point(index);
-    }
-    if (target == "next") {
-      index += 1;
-      animate(-imgWidth, target);
-      point(index);
-    }
-    // click points
-    if (event.target.tagName == "SPAN") {
-      let page = event.target.getAttribute("data-value");
-      list.style.left = -imgWidth * (page - 1) + "px";
-      point(page);
-    }
-  }, false);
-
-  // Event mouse
-  content.addEventListener("mouseover", (event) => {
-    clearInterval(timer);
-  }, false);
-  content.addEventListener("mouseout", (event) => {
-    timer = setInterval(function() {
-      index += 1;
-      animate(-imgWidth);
-      point(index);
-    }, 3000);
-  }, false);
 })();
+
+window.addEventListener("load", function(event) {
+  get_animate._timer = setInterval(function() {
+    get_animate.animate(-get_animate._width);
+    get_animate.point();
+  }, 3000);
+}, false);
+
+get_animate._box.addEventListener("click", function(event) {
+  let target = event.target.classList[0];
+  if (target == "prev") {
+    get_animate.animate(get_animate._width, target);
+    get_animate.point();
+  }
+  if (target == "next") {
+    get_animate.animate(-get_animate._width, target);
+    get_animate.point();
+  }
+  // click points
+  if (event.target.tagName == "SPAN") {
+    let page = event.target.getAttribute("data-value");
+    get_animate._list.style.left = -get_animate._width * (page - 1) + "px";
+    get_animate.point(page);
+  }
+}, false);
+
+// Event mouse
+get_animate._box.addEventListener("mouseover", (event) => {
+  clearInterval(get_animate._timer);
+}, false);
+get_animate._box.addEventListener("mouseout", (event) => {
+  get_animate._timer = setInterval(function() {
+    get_animate.animate(-get_animate._width);
+    get_animate.point();
+  }, 3000);
+}, false);
