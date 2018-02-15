cec65cfaaf75931149d37454b8622ed1691b07b7
Bowie <mr_lbw@outlook.com>
2018-02-15 15:17:25 +0800

    重构：加入Generation 函数

--- Clock-V2/Clock-V2.js
+++ Clock-V2/Clock-V2.js
@@ -17,6 +17,7 @@ let app = (function() {
     _min = $('.min'),
     _sec = $('.sec'),
     time = [_hour[0], _hour[1], _min[0], _min[1], _sec[0], _sec[1]];
+  let ticking;
 
   return {
 
@@ -37,32 +38,57 @@ let app = (function() {
       this.setAttribute('data-value', nowTime);
     },
 
+    // ES6 Generation函数具有惰性求值（有暂停态）的特点，在语义上更适合此处“闪烁圆点”
+    tickingPoint: function* () {
+      const point = $('.point');
+      while (true) {
+        for (let item of point) {
+          item.classList.add('on');
+        }
+        yield;
+
+        for (let item of point) {
+          item.classList.remove('on');
+        }
+        yield;
+      }
+    },
+
     runTime: function() {
 
       let now = /(\d)(\d):(\d)(\d):(\d)(\d)/.exec(new Date());
       [0, 1, 2, 3, 4, 5].forEach((i) => {
         app.setTime.call(time[i], now[i + 1]);
       });
-      // 位运算符速度快于普通数学运算符，这里判断奇偶数最低位是1或0
-      if (now[6] % 2 != 0) {
-        [0, 1, 2, 3].forEach((number) => {
-          $('.point')[number].classList.remove('on');
-        });
-      } else {
-        [0, 1, 2, 3].forEach((number) => {
-          $('.point')[number].classList.add('on');
-        });
+
+      if (!ticking) {
+        // 每次更新时，ticking变量是不能更新的，否则永远到不了第二个yield
+        ticking = app.tickingPoint();
       }
+      ticking.next();
+
+      // 方法二：闪烁圆点
+      // 原理：位运算符速度快于普通数学运算符，这里判断奇偶数最低位是1或0
+      // if (now[6] % 2 != 0) {
+      //   [0, 1, 2, 3].forEach((number) => {
+      //     $('.point')[number].classList.remove('on');
+      //   });
+      // } else {
+      //   [0, 1, 2, 3].forEach((number) => {
+      //     $('.point')[number].classList.add('on');
+      //   });
+      // }
     },
 
     // trigger
     trigger: function(evt) {
 
-      const runTime = app.runTime;  // 减少属性访问
+      const runTime = app.runTime; // 减少属性访问
       return new Promise((resolve, reject) => {
           resolve();
         })
         // Promise 类型用于解决“回调地狱”，此处后可使用then方法，用于未来补充回调函数
+        .then(runTime)  // 填补循环计时开始时的1秒空白
         .then((evt) => setInterval(runTime, 1000))
         .catch((err) => {
           console.log('Oops!Something wrong!Plese check your function trigger!');
